4911122c3d3654de227ff951abe1fe445753791b
Artem Zhuk <artem.zx656@gmail.com>
2023-02-17 20:43:06 +0300

    added @#$ checks

--- 1 module/econom/econom.go
+++ 1 module/econom/econom.go
@@ -4,13 +4,15 @@ import "fmt"
 
 func ops(input string) int {
 	inputRune := []rune(input)
-	startIndexes := make([]int, 0, len(input)/2) // stack of '(' indexes in inputRune slice
+	startIndexes := make([]int, 0, len(inputRune)/2) // stack of '(' indexes in inputRune slice
 	expressions := make(map[string]bool, 1)
-	for i := 0; i < len(input); i++ {
-		if input[i] == '(' {
+	for i := 0; i < len(inputRune); i++ {
+		if inputRune[i] == '(' && (inputRune[i+1] == '@' ||
+			inputRune[i+1] == '#' ||
+			inputRune[i+1] == '$') {
 			startIndexes = append(startIndexes, i)
 		}
-		if input[i] == ')' {
+		if inputRune[i] == ')' {
 			index := startIndexes[len(startIndexes)-1] // pops '(' index from stack,
 			startIndexes = startIndexes[:len(startIndexes)-1]
 			expressions[string(inputRune[index:i+1])] = true // adds "()" expression to the "expressions" map
